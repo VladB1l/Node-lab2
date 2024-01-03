3194361a6b59c756733ad0bc617a800d60ce2ff5
Vladyslav Bilokon <vlad2004bil@gmail.com>
2024-01-03 20:43:35 +0200

    Create server

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const i = 2 + 3;
-console.log(i);
+import http from 'http';
+import { parse } from 'url';
+import { handleRequest } from './router.js';
+
+const server = http.createServer((req, res) => {
+  const { pathname } = parse(req.url, true);
+  const method = req.method.toUpperCase();
+  handleRequest(pathname, method, req, res);
+});
+
+const PORT = 7100;
+
+server.listen(PORT, () => {
+  console.log(`Server is running: http://localhost:${PORT}`);
+});
+
+process.on('SIGTERM', () => {
+  console.log('Shutting down gracefully...');
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
